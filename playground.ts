81d64593adecfc874194cf9fb82a3779f3d53a62
Su Yiheng <827652549@qq.com>
2025-11-14 03:39:32 +0800

    完善for技巧

--- playground.ts
+++ playground.ts
@@ -1,56 +1,9 @@
-/**
- * 70. 爬楼梯
- * 已解答
- * 简单
- * 相关标签
- * premium lock icon
- * 相关企业
- * 提示
- * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
- *
- * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
- *
- *
- *
- * 示例 1：
- *
- * 输入：n = 2
- * 输出：2
- * 解释：有两种方法可以爬到楼顶。
- * 1. 1 阶 + 1 阶
- * 2. 2 阶
- * 示例 2：
- *
- * 输入：n = 3
- * 输出：3
- * 解释：有三种方法可以爬到楼顶。
- * 1. 1 阶 + 1 阶 + 1 阶
- * 2. 1 阶 + 2 阶
- * 3. 2 阶 + 1 阶
- *
- *
- * 提示：
- *
- * 1 <= n <= 45
- */
-
-// ----
-function climbStairs(n: number): number {
-  if (n === 2) return 2;
-  if (n === 1) return 1;
-  return climbStairs(n - 2) + climbStairs(n - 1)
-};
-
-function climbStairs(n: number): number {
-  const f = []
-  f[1] = 1;
-  f[2] = 2;
-  for (let i = 3; i <= n; i++) {
-    f[i] = f[i-1] + f[i-2];
+function maxProfit(prices: number[]): number {
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+      for (let j = 0; j < prices.length - i -1; j++) {
+        maxProfit = Math.max(maxProfit, prices[i+j+1] - prices[i]);
+      }
   }
-  return f[n]
-};
-console.log(climbStairs(3))
-console.log(climbStairs(45))
-
-
+  return maxProfit;
+}
