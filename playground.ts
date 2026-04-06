ea5774b787d5cd03366030bbc825f9dd42521998
Su Yiheng <827652549@qq.com>
2026-04-06 14:49:33 +0800

    55

--- playground.ts
+++ playground.ts
@@ -1,34 +1,18 @@
-// 📘 题目描述：
-// 给定一个经过编码的字符串，返回其解码后的结果。编码规则为：k[encoded_string] 表示其中方括号内部的字符串重复 k 次。
+function myNew(fn: Function, ...args: any[]) {
+  const obj = Object.create(fn.prototype)
 
-// 💡 示例：
-// 输入: "3[a2[c]]"
-// 输出: "accaccacc"
-
-// ⚙️ 测试用例：
-// 1️⃣ "3[a2[c]]" → "accaccacc"
-// 2️⃣ "2[ab]" → "abab"
-// 3️⃣ "2[a3[b]]" → "abbbabbb"
-// 4️⃣ "abc" → "abc"
-
-// 1️⃣ "x3[a2[c]]" → "xaccaccacc"
-
-
-function decode(str: string): string {
-  if (str.indexOf('[') === -1) return str
-  const m = Array.from(str.match(/^([a-z]*)(\d*)\[(.*)\]$/) || [])
-  const prefix = m[1]
-  const num = m[2] || '1'
-  const subStr = m[3] || ''
-  return prefix + decode(subStr).repeat(parseInt(num))
+  const r = fn.call(obj, ...args)
+  if ((typeof r === 'object' && r !== null) || typeof r === 'function') {
+    return r
+  }
+  return obj
 }
 
-
-console.log(decode('3[a2[c]]'))
-console.log(decode('2[ab]'))
-console.log(decode("2[a3[b]]"))
-console.log(decode('abc'))
-// 1️⃣ "3[a2[c]]" → "accaccacc"
-// 2️⃣ "2[ab]" → "abab"
-// 3️⃣ "2[a3[b]]" → "abbbabbb"
-// 4️⃣ "abc" → "abc"
+function myInstanceof(target: object | null, fn: Function): boolean {
+  let proto = target
+  while (proto !== null) {
+    if (proto === fn.prototype) return true
+    proto = Reflect.getPrototypeOf(proto)
+  }
+  return false
+}
